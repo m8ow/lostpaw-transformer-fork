efc15dc5a066d5794303c36eee8803a145678746
m8ow <phoneta1018@gmail.com>
2025-05-17 20:28:05 +0900

    [dataset] 元々のコードにタイポがあったので修正

--- lostpaw/data/__init__.py
+++ lostpaw/data/__init__.py
@@ -1,4 +1,5 @@
 from .extract_pets import DetrPetExtractor
 from .dataset import RandomPairDataset
+from .dataset import PetImageDataset
 
 __all__ = ["DetrPetExtractor", "RandomPairDataset", "PetImageDataset"]
